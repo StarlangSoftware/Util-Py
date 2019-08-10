5e7417b8e653fc0769399ef2d24e5c059c619c3a
Olcay Taner YILDIZ <olcaytaner@isikun.edu.tr>
2019-08-10 09:08:28 +0300

    Updated version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='NlpToolkit-Util',
-    version='1.0.0',
+    version='1.0.1',
     packages=['Util'],
     url='https://github.com/olcaytaner/Util-Py',
     license='',
