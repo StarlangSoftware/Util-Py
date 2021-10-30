32769ea3562b9046947053edf68ea9e25961ab8d
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2021-10-30 18:48:55 +0300

    Added Readme file to long description.

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 from setuptools import setup
 
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name='NlpToolkit-Util',
-    version='1.0.5',
+    version='1.0.6',
     packages=['Util'],
     url='https://github.com/StarlangSoftware/Util-Py',
     license='',
     author='olcaytaner',
     author_email='olcay.yildiz@ozyegin.edu.tr',
-    description='Simple Utils'
+    description='Simple Utils',
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
